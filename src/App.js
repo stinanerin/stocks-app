51e2c16887a2b66b89d864261e9e069bc8d659bc
stormstina <stina.nerin@gmail.com>
2023-08-24 14:59:04 +0200

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import StockOverviewPage from "./components/pages/StockOverviewPage";
+import StockDetailPage from "./components/pages/StockDetailPage";
+import NotFoundPage from "./components/pages/NotFoundPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={StockOverviewPage} />
+                    <Route
+                        exact
+                        path="/detail/:symbol"
+                        component={StockDetailPage}
+                    />
+                    <Route path="/*" component={NotFoundPage} />
+                </Switch>
+            </Router>
+        </main>
+    );
 }
 
 export default App;
