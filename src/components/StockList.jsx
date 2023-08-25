f638b703531c901504cdbc8880af763700e9e1eb
stormstina <stina.nerin@gmail.com>
2023-08-25 15:34:33 +0200

    Add rendering of WatchList

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
 import finnHub from "../services/finnHub";
 
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
+
 const StockList = () => {
     const [stock, setStock] = useState([]);
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+
+    const changeColor = (num) => (num < 0 ? "danger" : "success");
+    const renderIcon = (num) =>
+        num < 0 ? <BsFillCaretDownFill /> : <BsFillCaretUpFill />;
 
     useEffect(() => {
         let isMounted = true;
@@ -16,7 +22,6 @@ const StockList = () => {
                         },
                     });
                 });
-                // Equals to --> "https://finnhub.io/api/v1/quote?token=cjjkhr1r01qirue6aoigcjjkhr1r01qirue6aoj0&symbol=MSFT"
 
                 const response = await Promise.all(stocksReq);
 
@@ -46,7 +51,42 @@ const StockList = () => {
         // Only fetch data when the component mounts
     }, []);
 
-    return <div>StockList</div>;
+    return (
+        <table className="table hover mt-5">
+            <thead style={{ color: "rgb(79,89,102)" }}>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Chg</th>
+                    <th scope="col">Chg%</th>
+                    <th scope="col">High</th>
+                    <th scope="col">Low</th>
+                    <th scope="col">Open</th>
+                    <th scope="col">Pclose</th>
+                </tr>
+            </thead>
+            <tbody>
+                {stock.map(({ stock, data: { c, d, dp, h, l, o, pc } }) => {
+                    return (
+                        <tr className="table-row" key={stock}>
+                            <th>{stock}</th>
+                            <td>{c}</td>
+                            <td className={`text-${changeColor(d)}`}>
+                                {d} {renderIcon(d)}
+                            </td>
+                            <td className={`text-${changeColor(dp)}`}>
+                                {dp} {renderIcon(dp)}
+                            </td>
+                            <td>{h}</td>
+                            <td>{l}</td>
+                            <td>{o}</td>
+                            <td>{pc}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 };
 
 export default StockList;
--- src/services/finnHub.js
+++ src/services/finnHub.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// "https://finnhub.io/api/v1/quote?symbol=AAPL&token=cjjkhr1r01qirue6aoigcjjkhr1r01qirue6aoj0";
-
 import dotenv from "dotenv";
 dotenv.config();
 const TOKEN = process.env.TOKEN;
