8ea140ec8c82d445690df3218a1b168d014fd1af
stormstina <stina.nerin@gmail.com>
2023-08-25 15:30:38 +0200

    Add rendering of watchlist

--- src/services/finnHub.js
+++ src/services/finnHub.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // "https://finnhub.io/api/v1/quote?symbol=AAPL&token=cjjkhr1r01qirue6aoigcjjkhr1r01qirue6aoj0";
 
-const TOKEN = "cjjkhr1r01qirue6aoigcjjkhr1r01qirue6aoj0";
+import dotenv from "dotenv";
+dotenv.config();
+const TOKEN = process.env.TOKEN;
 
 export default axios.create({
     baseURL: "https://finnhub.io/api/v1",
