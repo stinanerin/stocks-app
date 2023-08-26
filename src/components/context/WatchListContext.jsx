7e389a59e9e90a77aaa0129a474560ac08b11820
stormstina <stina.nerin@gmail.com>
2023-08-26 15:13:43 +0200

    Add addStock & deletestock methods

--- src/components/context/WatchListContext.jsx
+++ src/components/context/WatchListContext.jsx
@@ -5,10 +5,23 @@ export const WatchListContext = createContext();
 export const WatchListContextProvider = (props) => {
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
 
+    const addStock = (stock) => {
+        if (watchList.indexOf(stock) === -1) {
+            setWatchList([...watchList, stock]);
+        }
+    };
+
+    const deleteStock = (stock) => {
+        const filteredWatchList = watchList.filter((el) => {
+            return el !== stock;
+        });
+        setWatchList(filteredWatchList);
+    };
+
     return (
-        <WatchListContext.Provider value={{ watchList }}>
+        <WatchListContext.Provider value={{ watchList, addStock, deleteStock }}>
             {
-                // Instructs context provider to render all children props
+                // Instructs context provider to render all children element props
             }
             {props.children}
         </WatchListContext.Provider>
